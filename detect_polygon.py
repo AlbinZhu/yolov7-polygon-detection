247cb1e577e9d8c719fe0a070cb15f05513d3c0d
Albin <albin.zhu.me@gmail.com>
2022-08-22 18:30:25 +0800

    bugfix for detect_polygon.py

--- detect_polygon.py
+++ detect_polygon.py
@@ -58,7 +58,7 @@ def detect(save_img=False):
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    # colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
     # Run inference
     if device.type != 'cpu':
